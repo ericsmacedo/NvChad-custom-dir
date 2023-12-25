4bcd9a2470053658c1964fca2c96c779f742e0b3
Eric Macedo <ericsmacedo@gmail.com>
2023-12-25 16:23:35 +0000

    Initial commit of NvChad custom files

---
+++ init.lua
@@ -0,0 +1,58 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+-- Vim has 3 kinds of options: global, buffer-local and window-local
+local o = vim.o  	-- Global options
+local wo = vim.wo	-- window-local options
+
+-- o.wildignore = "+=*/tmp/*,*.so,*.swp,*.zip,"   -- MacOSX/Linux
+o.scrolloff = 5       -- Show at least 5 lines
+
+-- window-local options
+wo.relativenumber = true	-- use relative numbers
+wo.colorcolumn = "80"		  -- columnt to indicate end of 80 characters
+wo.conceallevel = 0       -- Determine how text with the "conceal" syntax attribute :syn-conceal is shown
+wo.foldmethod = 'marker'
+
+-- buffer local options
+o.softtabstop = 4         -- number of spaces in tab when editing.
+o.tabstop = 4             -- number of spaces in tab when editing
+o.expandtab = true        -- tabs are spaces
+o.shiftwidth = 4          -- number of spaces used for >> and << 
+
+-- -- To prevent errors with the Shada (shared data) file, I added the line below
+-- vim.cmd[[ "set viminfo+=n~/vim/viminfo"]]
+-- 
+-- Specify which python environment is to be used with neovim
+vim.g.python3_host_prog = vim.call("expand", "/home/emacedo/.config/nvim/pyenv/bin/python3.9")
+
+-- "===================================================================
+-- " CUSTOM INDENTATION
+-- "===================================================================
+local set_ident = function()
+    vim.o.tabstop = 4          -- number of spaces in tab when editing
+    vim.o.softtabstop = 4      -- number of spaces in tab when editing.
+    vim.o.shiftwidth = 4       -- number of spaces used for >> and <<
+    vim.o.expandtab = true     -- tabs are spaces
+end
+
+local set_yml_ident = function()
+    vim.o.tabstop = 2          -- number of spaces in tab when editing
+    vim.o.softtabstop = 2      -- number of spaces in tab when editing.
+    vim.o.shiftwidth = 2       -- number of spaces used for >> and <<
+end
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"},
+                            {pattern = {"*.py", "*.sv", "*.cpp"},
+                             callback = set_ident
+                            })
+
+vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"},
+                            {pattern = {"*.yml"},
+                             callback = set_yml_ident
+                            })
